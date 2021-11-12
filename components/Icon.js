08d41f397971e0e548dd594143def466632d11a5
Alan Jenkins <alan@functionfirst.co.uk>
2021-11-12 17:50:11 +0000

    text-current isn't required

--- components/Icon.js
+++ components/Icon.js
@@ -2,7 +2,7 @@ const Icon = ({ children, className }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className={`text-current ${className}`}
+      className={className}
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
