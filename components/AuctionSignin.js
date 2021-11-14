b0534c162d8072e3abff6cbf135876593bc37758
Alan Jenkins <alan@functionfirst.co.uk>
2021-11-14 10:34:17 +0000

    Add redirect path to auction from signing button

--- components/AuctionSignin.js
+++ components/AuctionSignin.js
@@ -1,15 +1,17 @@
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { supabase } from '@/lib/initSupabase'
 
 function AuctionSignin ({ className }) {
   const user = supabase.auth.user()
+  const { asPath } = useRouter()
 
   if (user) {
     return null
   }
 
   return (
-    <Link href="/login">
+    <Link href={`/login?redirect=${asPath}`}>
       <a className={`${className} py-2 px-3 rounded-sm block text-center border-transparent bg-indigo-600 text-white hover:bg-indigo-800`}>
         Sign in to bid
       </a>
