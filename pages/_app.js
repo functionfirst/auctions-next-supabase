420e7fd131c7fb584323d109d5b692c53142b150
Alan Jenkins <alan@functionfirst.co.uk>
2021-11-17 16:03:18 +0000

    Add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
   if (Component.authRequired) {
     app = <AuthRequired>{app}</AuthRequired>
   }
-1
+
   return (
     <>
       <Head>
@@ -28,6 +28,12 @@ function MyApp({ Component, pageProps }) {
           content="Realtime Auctions app using next.js and supabase"
           key="description"
         />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       <UserContextProvider supabase={supabase}>
